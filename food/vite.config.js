81da4b80ddea5932412de455b1e3505b3dd67328
Kanhaiya Kumar <115523368+kanhaiyakumar11@users.noreply.github.com>
2025-04-20 12:06:18 +0530

    Update vite.config.js

--- food/vite.config.js
+++ food/vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// vite.config.js
+export default {
+  base: '/',
+  // ...rest of config
+}
+
 export default defineConfig({
   plugins: [react()],
 })
